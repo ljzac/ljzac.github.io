f88edb753d240d02b12950507932096f84cc23cc
Leonel Perea <iticleonel.len@gmail.com>
2013-05-15 12:22:01 -0500

    ref #2 Solución de compatibilidad de js

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -1,85 +1,49 @@
 $(document).ready(function(){
-    $(function(){
-      SyntaxHighlighter.all();
-    });
-
-      $('.carousels').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: true,
-        slideshow: false,
-        itemWidth:82,
-        itemMargin:18,
-        asNavFor: '.slider'
-      });
-     
-       $('.carouse-bottom').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: true,
-        slideshow: false,
-        itemWidth:229,
-        itemMargin:18
-      });
- 
-      $('.slider').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: true,
-        slideshow: false,
-        sync: ".carousels",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-      
-      //ipod nav 
-      
-      $(".touch span + span").click(function(){
-	 $(".ipod-nav > ul").slideToggle();     
-      });
- });
- 
- //contten scroll function
-jQuery(document).ready(function() {
-	jQuery('.first-and-second-carousel').jcarousel();
-  jQuery('.mycarousel').jcarousel({
-    vertical: true,
-    scroll: 1,
-	  wrap: null,
-	  auto:500
+  $(function(){
+    SyntaxHighlighter.all();
   });
 
+  $('.carousels').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: false,
+    itemWidth:82,
+    itemMargin:18,
+    asNavFor: '.slider'
+  });
+ 
+   $('.carouse-bottom').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: false,
+    itemWidth:229,
+    itemMargin:18
+  });
 
-/**
- * We use the initCallback callback
- * to assign functionality to the controls
- */
-function mycarousel2_initCallback(carousel) {
-    jQuery('#mycarousel2-next').bind('click', function() {
-        carousel.next();
-        return false;
+  $('.slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: false,
+    sync: ".carousels",
+    start: function(slider){
+      $('body').removeClass('loading');
+    }
+  });
+
+  $('.first-and-second-carousel').jcarousel();
+    $('.mycarousel').jcarousel({
+      vertical: true,
+      scroll: 1,
+      wrap: null,
+      auto:500
     });
 
-    jQuery('#mycarousel2-prev').bind('click', function() {
-        carousel.prev();
-        return false;
-    });
-};
+  $(".touch span + span").click(function(){
+    $(".ipod-nav > ul").slideToggle();     
+  });
 
-    jQuery('.mycarousel2').jcarousel({
-      visible: 4,
-        scroll: 1,
-        initCallback: mycarousel2_initCallback,
-        // This tells jCarousel NOT to autobuild prev/next buttons
-        buttonNextHTML: null,
-        buttonPrevHTML: null
-    });
-    
-    //font resize function
-      $('.font-resize').jfontsize({
-                            btnMinusClasseId: '#jfontsize-m',
-                            btnDefaultClasseId: '#jfontsize-d',
-                            btnPlusClasseId: '#jfontsize-p'
-                        });
 });
+      
